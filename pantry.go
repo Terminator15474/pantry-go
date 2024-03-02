8ac6d24f2113642c31d296b5fd70d10d673065c2
Terminator15474 <jandommert8@gmail.com>
2024-03-02 01:47:20 +0100

    added doc strings

--- pantry.go
+++ pantry.go
@@ -9,13 +9,28 @@ import (
 )
 
 type Pantry struct {
-	GetDetails    func() PantryInfo
+	// GetDetails returns the details of the pantry
+	GetDetails func() PantryInfo
+
+	// UpdateDetails updates the details of the pantry
 	UpdateDetails func(info UpdatedInfo) PantryInfo
 
+	// CreateOrReplaceBasket creates or replaces a basket in the pantry
+	// with the given name and data and returns true if successful
 	CreateOrReplaceBasket func(name string, data any) bool
-	UpdateBasketContent   func(name string, data any) any
-	GetBasketContent      func(name string, format any) any
-	DeleteBasket          func(name string) bool
+
+	// UpdateBasketContent updates the content of the basket
+	// with the given name and returns the updated data
+	UpdateBasketContent func(name string, data any) any
+
+	// GetBasketContent returns the content of the basket
+	// with the given name in the given format
+	GetBasketContent func(name string, format any) any
+
+	// DeleteBasket deletes the basket with the given name
+	// and returns true if successful
+	// THIS WILL DELETE ALL THE DATA IN THE BASKET
+	DeleteBasket func(name string) bool
 }
 
 type BasketInfo struct {
@@ -37,6 +52,7 @@ type UpdatedInfo struct {
 	Description string `json:"description"`
 }
 
+// CreatePantry creates a new Pantry API Wrapper with the given API key
 func CreatePantry(apiKey string) Pantry {
 	var url = "https://getpantry.cloud/apiv1/pantry/" + apiKey
 
